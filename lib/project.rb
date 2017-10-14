827518b3810bee08968c4d4db90c4b87bb2f9cdb
Kristen Peck <kpeck71@gmail.com>
2017-10-14 21:12:00 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,7 @@
 class Project
   attr_accessor :backers #so that projects can add backers to the list of backers and report on the backers who support them
 
-
+  def initialize
+    @backers = backers
+  end
 end
