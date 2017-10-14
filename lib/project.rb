b24bb23bb2d3d3a0793623dc53d5cf2c0a820079
Kristen Peck <kpeck71@gmail.com>
2017-10-14 21:14:36 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,7 @@
 class Project
   attr_accessor :title, :backers #so that projects can add backers to the list of backers and report on the backers who support them
 
-  def initialize
+  def initialize(title)
     @backers = backers
   end
 
