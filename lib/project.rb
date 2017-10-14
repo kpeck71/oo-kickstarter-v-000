5b5619920b4481722d0861aaa42907b49b04d5c3
Kristen Peck <kpeck71@gmail.com>
2017-10-14 21:18:52 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,7 @@ class Project
 
   def add_backer(backer)
     @backers << backer
-    backer.add_project(self) unless project.backers.include?(self)
+    backer.add_project(self) unless backer.projects.include?(self)
   end
 
 end
